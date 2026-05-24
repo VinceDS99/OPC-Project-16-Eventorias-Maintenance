76736c9d68abdd929ce2e60a526f58d45bc558d7
Vincent Dos Santos <vincedos@hotmail.com>
2026-05-24 19:30:26 +0200

    Bug fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,22 @@ plugins {
     alias(libs.plugins.google.services) apply false
     id("org.sonarqube") version "4.4.1.3373"
 }
+
+sonar {
+    properties {
+        property("sonar.projectKey",   "VinceDS99_OPC-Project-16-Eventorias-Maintenance")
+        property("sonar.organization", "vinceds99")
+        property("sonar.host.url",     "https://sonarcloud.io")
+        property("sonar.sources",      "src/main/java")
+        property("sonar.tests",        "src/test/java,src/androidTest/java")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${project.projectDir}/app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
+        )
+        property(
+            "sonar.exclusions",
+            "**/R.class,**/R\$*.class,**/BuildConfig.*,**/Manifest*.*," +
+                    "**/*_HiltModules*,**/*_Factory*,**/*Hilt*,**/*ComposableSingletons*"
+        )
+    }
+}
