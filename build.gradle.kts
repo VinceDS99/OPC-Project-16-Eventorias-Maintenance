0f8fdcd5d3d16340ffcee286f66a75f21fb89a68
Vincent Dos Santos <vincedos@hotmail.com>
2026-05-25 04:41:02 +0200

    ci: add CD workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,17 @@ plugins {
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.google.services) apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey",   "VinceDS99_OPC-Project-16-Eventorias-Maintenance")
+        property("sonar.organization", "vinceds99")
+        property("sonar.host.url",     "https://sonarcloud.io")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${project.projectDir}/app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
+        )
+    }
 }
